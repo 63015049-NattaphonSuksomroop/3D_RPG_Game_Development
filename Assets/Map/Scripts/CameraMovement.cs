5e7a4552ec54df04c48e13a778d8dd8552a20705
63015098 <104320349+63015098@users.noreply.github.com>
2022-11-13 11:27:03 +0700

    Map
    Map easy to hard

---
+++ Assets/Map/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private const float SMOOTH_TIME = 0.3f;
+    public bool LockX, LockY, LockZ;
+    public float offSetZ = -1f;
+    public bool useSmoothing = true;
+    public Transform target;
+    //Player
+    private Transform thisTransform;
+    //Camera
+    private Vector3 velocity; //การเคลื่อนที่ของกล้อง 
+
+    void Awake()
+    {
+        thisTransform = transform;
+        velocity = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+        var newPos = Vector3.zero;
+        if (useSmoothing)
+        {
+            //smooth
+            newPos.x = Mathf.SmoothDamp(thisTransform.position.x, target.position.x, ref velocity.x, SMOOTH_TIME);
+            newPos.y = Mathf.SmoothDamp(thisTransform.position.y, target.position.y, ref velocity.y, SMOOTH_TIME);
+            newPos.z = Mathf.SmoothDamp(thisTransform.position.z, target.position.z + offSetZ, ref velocity.z, SMOOTH_TIME);
+        }
+        else
+        {
+            newPos.x = target.position.x;
+            newPos.x = target.position.y;
+            newPos.x = target.position.z ;
+        }
+        if (LockX)
+        {
+            newPos.x = thisTransform.position.x;
+        }
+        if (LockY)
+        {
+            newPos.y = thisTransform.position.y;
+        }
+        if (LockZ)
+        {
+            newPos.z = thisTransform.position.z;
+        }
+        transform.position = Vector3.Slerp(thisTransform.position, newPos, Time.time);
+    }
+}
---
+++ Assets/Map/Scripts/CharacterMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Animator animator; 
+    CharacterController characterController; //เก็บค่า component
+
+    public float speed = 6.0f; 
+    public float rotationSpeed = 25; //การหมุน
+    public float jumpSpeed = 7.5f; 
+    public float graviy = 20.0f; //แรงโน้มถ่วง
+    Vector3 inputVec; //แกนที่จะวิ่ง จะรับค่า x,y,z เคลื่อนที่ movement
+    Vector3 targetDirection; //ทิศทางที่จะไปจริงๆ 
+
+    private Vector3 moveDirection = Vector3.zero; //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = -(Input.GetAxisRaw("Vertical"));
+        float z = Input.GetAxisRaw("Horizontal");
+        inputVec = new Vector3(x, 0, z);
+
+        animator.SetFloat("Input X", z);
+        animator.SetFloat("Input Z", -(x));
+
+        if (x != 0 || z != 0)
+        {
+            animator.SetBool("Moving", true);
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+            animator.SetBool("Running", false);
+        }
+
+        //Jump
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+        UpdateMovement();
+
+    }
+    void UpdateMovement()
+    {
+        Vector3 motion = inputVec;
+        motion *= (Mathf.Abs(inputVec.x) == 1 && Mathf.Abs(inputVec.z) == 1) ? .7f : 1;
+        RotateTowardMovementDirection();
+        getCameraRealtive();
+    }
+    void RotateTowardMovementDirection()
+    {
+        if(inputVec != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * rotationSpeed);
+        }
+    }
+    void getCameraRealtive()
+    {
+        Transform cameraTransform = Camera.main.transform;
+        Vector3 forward = cameraTransform.TransformDirection(Vector3.forward);
+        forward.y = 0;
+        forward = forward.normalized;
+
+        Vector3 right = new Vector3(forward.z, 0, -forward.x);
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        targetDirection = (h * right) + (v * forward);
+    }
+}
